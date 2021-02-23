6c7677290eac4287ec1ee4806ca20e3e847348b4
최대현 <eogusalrns@naver.com>
2021-02-23 10:37:16 +0900

    CH04 완료

---
+++ chap_04/Chap04App/StringConcatApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StringConcatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(123 + 456);
+            Console.WriteLine("123" + "456");
+            Console.WriteLine(123 + "456");
+            Console.WriteLine($"{123}456");
+
+            int a = 30, b = 20;
+            Console.WriteLine(a < b);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a != b);
+
+            Console.WriteLine(a > b && a != b);
+            Console.WriteLine(a > b || a != b);
+
+            //조건연산자 (삼항)
+            int c = 100;
+            string result = c == 100 ? "백점" : "안백점";
+            Console.WriteLine(result);
+
+            Console.WriteLine("NULL 병합연산자");
+            int? d = null;
+            Console.WriteLine($"{d}");
+            Console.WriteLine($"{d ?? 0}");
+        }
+    }
+}
