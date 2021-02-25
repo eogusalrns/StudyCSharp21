2ea3bab47ece3976890c42ba52dec55d796228de
최대현 <eogusalrns@naver.com>
2021-02-25 12:30:59 +0900

    02_25 3교시 7장 마무리

---
+++ chap_07/Chap07App/OverrideTestApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideTestApp
+{
+    class ArmorSuite
+    {
+        //자식클래스에서 재정의할거다
+        public virtual void Initialize()
+        {
+            Console.WriteLine("ArmorSuite 초기화");
+        }
+    }
+
+    class IronMan : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("아모 슈트 장착");
+        }
+    }
+
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Double-Barrel 캐논");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ArmorSuite 생산");
+            ArmorSuite suite = new ArmorSuite();
+            suite.Initialize();
+
+            Console.WriteLine("Ironman  생산");
+            IronMan iron = new IronMan();
+            iron.Initialize();
+
+            Console.WriteLine("WarMachine 생산");
+            WarMachine machine = new WarMachine();
+            machine.Initialize();
+        }
+    }
+}
