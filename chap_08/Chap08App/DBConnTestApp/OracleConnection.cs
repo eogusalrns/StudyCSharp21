480a9b5dcbc14386623d726199f784f38f609e4d
최대현 <eogusalrns@naver.com>
2021-02-26 15:53:44 +0900

    8장 인터페이스 커밋 02_26 6교시

---
+++ chap_08/Chap08App/DBConnTestApp/OracleConnection.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace DBConnTestApp
+{
+    internal class OracleConnection : IDbConnection
+    {
+        private string connstring;
+
+        public OracleConnection(string connstring)
+        {
+            this.connstring = connstring;
+        }
+    }
+}
