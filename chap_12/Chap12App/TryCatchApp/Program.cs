c77fd1336242f0d7cfca1ae6af56d5de5cb24c3a
최대현 <eogusalrns@naver.com>
2021-03-02 17:25:52 +0900

    0302 8교시 커밋

---
+++ chap_12/Chap12App/TryCatchApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TryCatchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    i /= 0;
+                    Console.WriteLine($"{i} : {arr[i]}");
+                }
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"예외발생 : {ex.Message}");
+                //throw;
+            }
+            catch (Exception exo)
+            {
+                Console.WriteLine($"예외의 예외 : {exo.Message}");
+            }
+            Console.WriteLine("나머지 일처리 계소ㅓㄱ...");
+
+            try
+            {
+                int x = 108, y = 0;
+                int result = x / y;
+                Console.WriteLine(result);
+
+            }
+            catch (DivideByZeroException exe)
+            {
+                Console.WriteLine($"예외발생 : {exe.Message}");
+                //throw;
+            }
+            Console.WriteLine("다시 일처리 계소ㅓㄱ...");
+
+            string strVal = "hello World";
+            string splitVal = strVal.Substring(6, 5);
+            Console.WriteLine($"자른 문자 : {splitVal}");
+            //int index = strVal.IndexOf("W");
+            //Console.WriteLine($"W의 위치 {index}");
+        }
+    }
+}
